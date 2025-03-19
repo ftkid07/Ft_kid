1dbc7d303ac7f0b120369fed8c82ee424c98ade9
ftkid07 <eeyblack@gmail.com>
2025-03-19 17:22:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -74,8 +74,6 @@ const { autoStatusCommand, handleStatusUpdate } = require('./commands/autostatus
 // Global settings
 global.packname = settings.packname;
 global.author = settings.author;
-global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
 
 // Add this near the top of main.js with other global configurations
 const channelInfo = {
