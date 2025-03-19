a8752e9610f115c4cb18008652da262a9bbd2952
ftkid07 <eeyblack@gmail.com>
2025-03-19 17:19:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "_ft_kid",
+  botOwner: '_ft_kid', // Your name
+  ownerNumber: '918304002501', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
